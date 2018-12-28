74dbfb67183990b4e6f11f0b7c6181e3f47e3f00
Hayden Pope <stilbruch39@gmail.com>
2018-12-28 13:24:46 -0600

    Generalized the GUI into it's own class to seperate GUI logic from calculator logic

---
+++ src/main/java/com/stilbruch/spigotcalc/gui/CalcGui.java
@@ -0,0 +1,25 @@
+package com.stilbruch.spigotcalc;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class CalcGui extends ItemGui {
+
+    public CalcGui() {
+        //Now the actually setup the calcualtor
+        fillColumn(0, blankItem(Material.BLACK_STAINED_GLASS_PANE), null);
+        fillColumn(1, blankItem(Material.BLACK_STAINED_GLASS_PANE), null);
+        fillColumn(7, blankItem(Material.BLACK_STAINED_GLASS_PANE), null);
+        fillColumn(8, blankItem(Material.BLACK_STAINED_GLASS_PANE), null);
+    }
+
+}
---
+++ src/main/java/com/stilbruch/spigotcalc/gui/ItemGui.java
@@ -0,0 +1,77 @@
+package com.stilbruch.spigotcalc.gui;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class ItemGui implements InventoryHolder {
+    
+    protected final int rows;
+    protected final int size;
+    private Inventory inv;
+    private Consumer<InventoryClickEvent>[] slotHandlers;
+
+    public ItemGui(int rows) {
+        this.rows = rows;
+        this.size = rows * 9;
+        inv = Bukkit.createInventory(this, size);
+        slotHandlers = new Consumer[size];
+    }
+
+    /**
+     * Displayes the calculator to a player
+     */
+    public void display(Player player) {
+        player.openInventory(inv);
+    }
+
+    /**
+     * This method handles when a player clicks the GUI. Keeping this inside the CalcGUI class keeps it's outward facing API clean
+     */
+    public void registerClick(final InventoryClickEvent clickEvent) {
+        //Check if the slot is valid
+        if (clickEvent.getSlot() < 0 || clickEvent.getSlot() > size){
+            return;
+        }
+        Consumer<InventoryClickEvent> clickHandler = slotHandlers[clickEvent.getSlot()];
+        if (clickHandler != null) {
+            clickHandler.accept(clickEvent);
+        }
+        clickEvent.setCancelled(true);
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inv;
+    }
+
+    /**
+     * returns an item stack with a blank name, so that the name in the GUI is not the item name
+     */
+    protected ItemStack blankItem(Material material) {
+        ItemStack stack = new ItemStack(material);
+        ItemMeta meta = stack.getItemMeta();
+        meta.setDisplayName(" ");
+        stack.setItemMeta(meta);
+        return stack;
+    }
+
+    protected void setItem(int slot, ItemStack item, Consumer<InventoryClickEvent> clickHandler) {
+        inv.setItem(slot, item);
+        slotHandlers[slot] = clickHandler;
+    }
+
+    protected void fillColumn(int column, ItemStack item, Consumer<InventoryClickEvent> clickHandler){
+        for (int i = 0; i < rows;i++) {
+            setItem((i * 9) + column, item, clickHandler);
+        }
+    }
+}
