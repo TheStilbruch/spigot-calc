76acd52cf054863ad9ecfb4bc997df34c3b76048
Hayden Pope <stilbruch39@gmail.com>
2018-12-28 14:17:16 -0600

    added support for all operators

--- src/main/java/com/stilbruch/spigotcalc/gui/CalcGui.java
+++ src/main/java/com/stilbruch/spigotcalc/gui/CalcGui.java
@@ -2,6 +2,8 @@ package com.stilbruch.spigotcalc.gui;
 
 import java.util.function.Consumer;
 
+import javax.script.ScriptException;
+
 import com.stilbruch.spigotcalc.SpigotCalcPlugin;
 import com.stilbruch.spigotcalc.util.ItemUtils;
 
@@ -12,12 +14,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+
 public class CalcGui extends ItemGui {
+
+    private static final String DEFAULT_TITLE = "Calculator: ";
 
     private final SpigotCalcPlugin plugin;
 
     public CalcGui(SpigotCalcPlugin plugin) {
-        super(3, "Calculator: ");
+        super(3, DEFAULT_TITLE);
 
         this.plugin = plugin;
 
@@ -27,12 +33,41 @@ public class CalcGui extends ItemGui {
         fillColumn(7, blankItem(Material.BLACK_STAINED_GLASS_PANE), null);
         fillColumn(8, blankItem(Material.BLACK_STAINED_GLASS_PANE), null);
 
+        //Numbers 1-9
         for (int i = 1;i < 10;i++) {
-            int slot = ((i - 1) % 3) + ((i - 1) / 3) * 9 + 2; // Lot's of maths to get this in the right spot
+            int slot = ((i - 1) % 3) + 2 + ((i - 1) / 3) * 9; // Lot's of maths to get this in the right spot
             setItem(slot, getMenuItem(Material.EMERALD, String.valueOf(i)), getClickHandler(String.valueOf(i)));
         }
+        setItem(23, getMenuItem(Material.EMERALD, "0"), getClickHandler("0")); //Number 0
 
+        //Add the operators
         setItem(5, getMenuItem(Material.REDSTONE, "+"), getClickHandler("+"));
+        setItem(6, getMenuItem(Material.REDSTONE, "-"), getClickHandler("-"));
+        setItem(14, getMenuItem(Material.REDSTONE, "*"), getClickHandler("*"));
+        setItem(15, getMenuItem(Material.REDSTONE, "/"), getClickHandler("/"));
+
+        //Clear button
+        setItem(size - 1, getMenuItem(Material.BARRIER, ChatColor.RED + "CLEAR"), clickEvent -> {
+            new CalcGui(plugin).display((Player) clickEvent.getWhoClicked());
+        });
+
+        //And finally the equals button
+        setItem(24, getMenuItem(Material.REDSTONE, "="), clickEvent -> {
+            ItemGui gui = (ItemGui) clickEvent.getInventory().getHolder();
+            Player player = (Player) clickEvent.getWhoClicked();
+
+            try {
+                Object rawSolution = plugin.engine.eval(gui.title.replace(DEFAULT_TITLE, ""));
+                double solution = Double.parseDouble(rawSolution.toString());
+
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 2f, 1f);
+                new ItemGui(gui, DEFAULT_TITLE + String.valueOf(solution)).display(player); //Change the title to have the solution
+            } catch (ScriptException e) {
+                //TODO: Handler
+                e.printStackTrace();
+            }
+            
+        });
     }
 
     private ItemStack getMenuItem(Material mat, String name) {
